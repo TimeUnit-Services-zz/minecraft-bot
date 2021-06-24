514345d1499ff7f0836f4e7b2d3035302b05f8ad
rHCO <65933348+rHCO-rDevelopment@users.noreply.github.com>
2021-06-23 23:10:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Subscribe To Easy Gamer : https://www.youtube.com/channel/UCk575VDlrCsv1Gy2XZQ1bSA")
+	bot.chat("hi")
 	first = false;
 	}
 	else{
-		bot.chat("Subscribe To MannuG : https://www.youtube.com/channel/UCSZHRnBue50ImBKeR6k6eDg")
+		bot.chat("hi")
 		first = true;
 	}
   }, 3600000 * i); 
